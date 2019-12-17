f12840faeacec74c33d1c257223b8fd3f1d1629c
Ryan <ryan.fee625@gmail.com>
2019-12-17 14:03:11 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "scalr_aws_secret_key" {}
+
+variable "scalr_aws_access_key" {}
+
+variable "region" {
+  description = "Region"
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+}
+
+variable "subnet" {
+description = "Subnet ID"
+}
+
+variable "sg" {
+  description = "AWS Secruity Group"
+  type        = list(string)
+}
+
+variable "key" {
+  description = "AWS Key"
+}
+
+variable "vpc_id" {
+description = "VPC"
+}
+
+variable "ami" {
+  description = "AMI"
+}
+
+variable "instance_name" {
+  description = "Name of the ec2 instance"
+}
+
+variable "sox" {
+  description = "Does this need to be sox compliant?"
+}
+
+variable "purpose" {
+  description = "What is the purpose of this server"
+}
